9b5e3f8c6a98d250f7cb82e5255583764f8c11ea
jonzappola <55258519+jonzappola@users.noreply.github.com>
2020-02-27 00:38:22 -0500

    Update GroceryDiscountV2.java

--- GroceryDiscountV2.java
+++ GroceryDiscountV2.java
@@ -9,41 +9,41 @@ public class GroceryDiscount {
 	    while(true){
 	       
 	        System.out.printf("%n Please enter the cost of your groceries or '0' to quit: ");
-		double price = scan.nextDouble();
+		    double price = scan.nextDouble();
 	        	
 	        	if(price>0&&price<10)
-		{
+		        {
 		        	System.out.print("You are not eligible for a coupon at this time.");
-		}
+		        }
 	        	else if(price>=10&&price<=60)
-		{
+		        {
 		        	coupon = price*.08;
 		           	System.out.printf("You win a discount coupon of $ %.2f",coupon);
 		        	System.out.print(" (8% of your purchase)");
-		}
+		        }
 	        	else if(price>60&&price<=150)
-		{
+		        {
 		        	coupon = price*.10;
 		        	System.out.printf("You win a discount coupon of $ %.2f",coupon);
 		        	System.out.print(" (10% of your purchase)");
-		}
+		        }
 	        	else if(price>150&&price<=210)
-		{
+		        {
 		        	coupon = price*.12;
 		        	System.out.printf("You win a discount coupon of $ %.2f",coupon);
 		        	System.out.print(" (12% of your purchase)");
-		}
+		        }
 	        	else if(price>210&&price<=1000)
-		{
+		        {
 		            	coupon = price*.14;
 		           	System.out.printf("You win a discount coupon of $ %.2f",coupon);
-                   	 	System.out.print(" (14% of your purchase)");
-		}
+                    		System.out.print(" (14% of your purchase)");
+		        }
 	        	else
-		{
+		        {
 	            	System.exit(1);
-		}
-	    }
+		        }
+        }
 		
 	
 		    
