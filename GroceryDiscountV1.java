f00f6d38ae3e6130a43060a1e2e4c27a1d60586d
jonzappola <55258519+jonzappola@users.noreply.github.com>
2020-02-27 00:50:06 -0500

    Update GroceryDiscountV1.java

--- GroceryDiscountV1.java
+++ GroceryDiscountV1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class GroceryDiscount {
+public class GroceryDiscountV1 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Please enter the cost of your groceries or '0' to quit: ");
