3878918152537aefa0c223f8e6c2aee506465606
jonzappola <55258519+jonzappola@users.noreply.github.com>
2020-03-15 20:32:49 -0400

    Update NOAATable.java

--- NOAATable.java
+++ NOAATable.java
@@ -1,4 +1,3 @@
-
 public class NOAATable
 {
 	public static void main(String args[])
@@ -8,17 +7,22 @@ public class NOAATable
 		for (T=80;T<=110;T+=2) 
 		{
 			System.out.printf("%5d",T);
+			
 		}
 		System.out.println();
 		System.out.println("______________________________________________________________________________________");
+		
 		for (int RH = 40; RH <= 100;RH+=5)
 		{
 			System.out.printf("%3d | ",RH);
+			T = 80;
 			for (int a = 1; a <= 16; a++)
 			{
 				System.out.printf("%5d",(int)(-42.379+2.04901523*T+10.14333127*RH-.22475541*T*RH-.00683783*T*T-.05481717*RH*RH+.00122874*T*T*RH+.00085282*T*RH*RH-.00000199*T*T*RH*RH));
+				T += 2;
 			}
 			System.out.println(); 
 		}
+		
 	}
 }
